3eb4712fddc9e1fbb1fe57739b08380dd569c54b
Filipe <filipeviegasribeiro@gmail.com>
2021-12-04 20:52:54 -0300

    function playRound

--- RPS.js
+++ RPS.js
@@ -1,7 +1,39 @@
+const str = ['rock', 'paper', 'scissors'];
 function computerPlay() {
-    let str = ['Rock', 'Paper', 'Scissors'];
     let ind = Math.floor(Math.random() * 3)
-    let res1 = str[ind]
-    return res1
+    let res = str[ind]
+    return res
 }
 
+function playRound(playerSelection, computerSelection) {
+    let res
+    if (typeof playerSelection !== 'string') {
+        res = "This is not a text, please insert 'rock', 'paper' or 'scissors'"
+    }
+    let ps = playerSelection.toLowerCase()
+    let cs = computerSelection
+
+    if (ps === cs) {
+        res = 'It\'s a tie, play again!'
+    }else if ((ps===str[0]) && (cs===str[1])) {
+        res = 'Paper beats Rock, You loose.'
+    }else if ((ps===str[0]) && (cs===str[2])) {
+        res = 'Rock beats Scissors, You win!.'
+    }else if ((ps===str[1]) && (cs===str[2])) {
+        res = 'Scissors beats Paper, You loose.'
+    }else if ((ps===str[1]) && (cs===str[0])) {
+        res = 'Paper beats Rock, You win!.'
+    }else if ((ps===str[2]) && (cs===str[0])) {
+        res = 'Rock beats Scissors, You loose.'
+    }else if ((ps===str[2]) && (cs===str[1])) {
+        res = 'Scissors beats Paper, You win!.'
+    } else {
+        res ="please insert 'rock', 'paper' or 'scissors'" 
+    }
+
+    return res
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
