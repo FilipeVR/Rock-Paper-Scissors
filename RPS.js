8262d473da5b94084bd424c3d2d7e2843688f2a1
Filipe <filipeviegasribeiro@gmail.com>
2022-03-21 22:14:48 -0300

    Add reset logic and create RPS.css
    Lock the user input buttons when computer or player reaches 5 match wins changing the method element.disable to True. Create Play Again button that reset the game to its initial state by changing the values of the necessary methods. Creates RPS.css to add style to the page.

--- RPS.js
+++ RPS.js
@@ -9,37 +9,50 @@ function computerPlay() {
 const sts = document.querySelector('#display span');
 
 function playRound(playerSelection, computerSelection) {
-    let res;
     let winner;
     let ps = playerSelection;
     let cs = computerSelection;
 
     if (ps === cs) {
-        sts.textContent = 'Game Status: It\'s a tie!' 
+        sts.textContent = 'Match Status: It\'s a tie!'
+        sts.setAttribute("style", "color: gray;"); 
         winner = 'none'
     }else if ((ps===str[0]) && (cs===str[1])) {
-        sts.textContent = 'Game Status: Paper beats Rock, You lost the match.' 
+        sts.textContent = 'Match Status: Paper beats Rock, You lost the match.'
+        sts.setAttribute("style", "color: red;"); 
         winner = 'computer'
     }else if ((ps===str[0]) && (cs===str[2])) {
-        sts.textContent = 'Game Status: Rock beats Scissors, You won the match!' 
+        sts.textContent = 'Match Status: Rock beats Scissors, You won the match!'
+        sts.setAttribute("style", "color: green;"); 
         winner = 'player'
     }else if ((ps===str[1]) && (cs===str[2])) {
-        sts.textContent = 'Game Status: Scissors beats Paper, You lost the match.' 
+        sts.textContent = 'Match Status: Scissors beats Paper, You lost the match.'
+        sts.setAttribute("style", "color: red;"); 
         winner = 'computer'
     }else if ((ps===str[1]) && (cs===str[0])) {
-        sts.textContent = 'Game Status: Paper beats Rock, You won the match!' 
+        sts.textContent = 'Match Status: Paper beats Rock, You won the match!'
+        sts.setAttribute("style", "color: green;"); 
         winner = 'player'
     }else if ((ps===str[2]) && (cs===str[0])) {
-        sts.textContent = 'Game Status: Rock beats Scissors, You lost the match.' 
+        sts.textContent = 'Match Status: Rock beats Scissors, You lost the match.'
+        sts.setAttribute("style", "color: red;"); 
         winner = 'computer'
     }else if ((ps===str[2]) && (cs===str[1])) {
-        sts.textContent = 'Game Status: Scissors beats Paper, You won the match!' 
+        sts.textContent = 'Match Status: Scissors beats Paper, You won the match!'
+        sts.setAttribute("style", "color: green;"); 
         winner = 'player'
     }
     
     return winner
 }
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('#button');
+
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+
+const playAgain = document.querySelector('.playAgain');
+playAgain.setAttribute("style", "display: none;");
 
 let playerWins = 0;
 let computerWins = 0;
@@ -51,7 +64,7 @@ const score =  document.querySelector('#score .score');
 Array.from(buttons).forEach((btn) => {
     btn.addEventListener('click', () => {
         let res = playRound(btn.getAttribute('class'), computerPlay())
-        console.log(res);
+        
         if (res === 'player') {
             playerWins += 1;
         } else if (res === 'computer') {
@@ -62,16 +75,36 @@ Array.from(buttons).forEach((btn) => {
         computer.textContent = `Computer: ${computerWins}`
 
         if ((playerWins === 5) || (computerWins === 5)) {
+            rock.disabled = true;
+            paper.disabled = true;
+            scissors.disabled = true;
+            playAgain.setAttribute("style", "display: block;");
             if (playerWins > computerWins) {
                 score.textContent = 'Congrats! You Won The Game!'
+                score.setAttribute("style", "color: green;");
             }
             else if (computerWins > playerWins) {
                 score.textContent = 'You Lost The Game. Try Again!'
+                score.setAttribute("style", "color: red;");
             }
         }
             })
     }
         );
+    
+    playAgain.addEventListener('click', () => {
+        playAgain.setAttribute("style", "display: none;");
+        rock.disabled = false;
+        paper.disabled = false;
+        scissors.disabled = false;
+        score.textContent = '';
+        sts.textContent = '';
+        player.textContent = '';
+        computer.textContent = '';
+        playerWins = 0;
+        computerWins = 0;
+    })
+
 
 
 /*function game() {
